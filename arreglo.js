600317d5034fbe5ba3b12608f6e62bf3c29fb69a
Juan Esteban <juanestebanochoa@gmail.com>
2020-09-29 23:00:43 -0300

    first array code

---
+++ arreglo.js
@@ -0,0 +1,8 @@
+function valores(valor1, valor2, valor3, valor4){
+    return [valor1, valor2, valor3, valor4];
+}
+
+let imp = valores("uno", "dos", "tres", "cuatro");
+
+console.log(imp);
+console.log (imp.length);
--- index.js
+++
@@ -1,7 +0,0 @@
-console.log('***iniciando mi primer programa en JS***');
-console.log('Ahora es de verdad!nodemon');
-
-console.log('Ejecutando programa');
-function bla(){
-    return 'OJO';
-};
