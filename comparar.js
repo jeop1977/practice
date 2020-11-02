7c23cfc49ddf9df98bcd0d1707a1fbb1aa0fedd4
Juan Esteban <juanestebanochoa@gmail.com>
2020-11-02 17:04:39 -0300

    Se modifican COMPARAR y UNIQUE

--- comparar.js
+++ comparar.js
@@ -1,18 +1,25 @@
 //Práctica isEqual
 const a = [3, true, "cuatro",7, 2, 9];
-const b = [3, true, "cuatro",7, 2, 9];
+const b = [3, "cuatro",7, 5, true, 9];
 
-function comparar(x, y){
-  return JSON.stringify(x) === JSON.stringify(y);
+function comp(a, b){
+  const arr1 = Array.from(a);
+  const arr2 = Array.from(b);
+  if (a.length !== b.length) return false;
+  
+  arr1.sort();
+  arr2.sort();
+
+  let bandera = true;
+
+  arr1.forEach((valor, indice) => {
+    if (valor !== arr2[indice]){
+      bandera = false;
+      return
+    }
+  });
+
+  return bandera;
 }
 
-console.log(comparar(a, b));
-
-/* Este fue un ensayo fallido:
-
-function comparar2(x, y){
-  return _.isEqual(x, y);
-}
-
-console.log(comparar2(a, b));
-*/
+console.log(comp(a, b));
--- unique.js
+++ unique.js
@@ -1,9 +1,41 @@
+//strict
+
 //Práctica Unique
-function unicos(valor, indice, matriz) { //Tengo dudas acerca del funcionamiento de estos parámetros
+function unicos(valor, indice, matriz) { 
+    //Tengo dudas acerca del funcionamiento de estos parámetros
     return matriz.indexOf(valor) === indice;
 }
 
-const itemes = [7, 12, "Juan", 77, 12, 32, "Juan", false, false, true, 44, 77, true]
+const itemes = [7, 12, "Juan", 77, 12, 12, 12, 12, 32, "Juan", false, false, true, 44, 77, true]
 const matrizUnica = itemes.filter(unicos)
 
+console.log(Array.isArray(itemes));
+
 console.log(matrizUnica);
+
+const items = [7, 12, "Juan", 77, 12, 32, "Juan", false, false, true, 44, 77, true]
+console.log('My Arrays:', items);
+
+const newArray = items.filter((v, i, a) => a.indexOf(v) === i)
+
+console.log(newArray)
+
+console.log('***************************')
+
+function fname(a, b, ...arg) {
+    console.log(a,b, arg)
+}
+
+const arr = [...itemes,  ...newArray]
+
+fname("Adrian", "Higuita", "Hola mundo", "QUe mas pues")
+console.log(arr)
+
+const obj = { firstName: "Adrian", lastName: "Higuita"}
+const obj2 = { age: 34, sex: "male" }
+
+console.log({id: "1234", info: obj2})
+
+obj2.age = 33;
+
+console.log({id: "1234", info: {...obj2}})
