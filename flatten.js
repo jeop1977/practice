c6ac0efe821ec3826a2b6cfa6a2c39a7332fde97
Juan Esteban <juanestebanochoa@gmail.com>
2020-11-11 01:42:49 -0300

    Se agrega flatten, pero falta arreglarlo

---
+++ flatten.js
@@ -0,0 +1,23 @@
+const arrIn = [9, 4, 12, [32, 41, [15, 77], 17], 33];
+let arrFn = arrIn.flat(2);
+
+console.log(arrFn);
+
+let arr2 = [];
+
+  for(let i = 0; i < arrIn.length; i++){
+    if(Array.isArray(arrIn[i])){
+      for(let a = 0; a < arrIn[i].length; a++){
+        if(Array.isArray(arrIn[i][a])){
+          for(let b = 0; b < arrIn[i][a].length; b++){
+            arr2.push(arrIn[i][a][b]);
+          }
+        }
+        arr2.push(arrIn[i][a]);
+      }
+    }
+    arr2.push(arrIn[i]);
+  }
+
+
+console.log(arr2);
